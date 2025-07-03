8e67d187a5c373b345a103d4565da78a298d4de0
Mikhaël Regni <mikhael@uping.be>
2025-07-03 16:51:36 +0200

    Remove list

--- boardgametracker.client/src/routes/index.tsx
+++ boardgametracker.client/src/routes/index.tsx
@@ -30,35 +30,6 @@ function RouteComponent() {
     <BgtPage>
       <BgtPageHeader header={t('common.dashboard')} actions={[]} />
       <BgtPageContent>
-        <h1>TODO</h1>
-        <ul>
-          <li>✅ Filter games on name (input field)</li>
-          <li>✅ Make tags clickable in game detail and navigate to game overview with filter</li>
-          <li>✅ Support for Expansions</li>
-          <li>Move badges for achievemnts out of $playerId.tsx</li>
-          <li>
-            User achievements:
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Played 5, 10, 50, 100 sessions
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- DONE - Played 3, 10, 20, 50 different games
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Won 3, 10, 25, 50 sessions
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Played for 5, 10, 50, 100 hours
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Have a win percentage of 30%, 50%, 70%, 90% (with minimum of 5 sessions
-            played)
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Solo Specialist: Win 5, 10, 25, 50 solo games
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Win 5, 10, 15, 25 games in a row
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Social Player: Play with 5, 10, 25, 50 different people
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Won with 2 or less points difference from second place
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Almost there!: Lost with 2 or less points difference from first place
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Marathon Runner: Play a single game session lasting 4+ hours
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- First Try: Win a game on your first play
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Learning Curve: Beat your personal best score 3 times in the same game
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Monthly Goal: Play 20+ games in a single month
-            <br /> &nbsp;&nbsp;&nbsp;&nbsp;- Consistent Schedule: Play every Saturday for 10 weeks straight
-          </li>
-          <li>REMOVE ME WHEN ALL IS DONE</li>
-        </ul>
-        <h1>Bugs</h1>
-        <ul></ul>
         <div className="grid grid-cols-3 lg:grid-cols-4 gap-1 md:gap-3">
           <BgtTextStatistic content={statistics.gameCount} title={t('statistics.game-count')} />
           <BgtTextStatistic content={statistics.expansionCount} title={t('statistics.expansion-count')} />
