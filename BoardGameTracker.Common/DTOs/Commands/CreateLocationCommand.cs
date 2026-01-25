4e8e461e18dc18d06bba78a326ca5d3b78c7658b
Mikhaël Regni <mikhael@uping.be>
2026-01-25 12:51:42 +0100

    Removing more validation

--- BoardGameTracker.Common/DTOs/Commands/CreateLocationCommand.cs
+++ BoardGameTracker.Common/DTOs/Commands/CreateLocationCommand.cs
@@ -4,8 +4,6 @@ namespace BoardGameTracker.Common.DTOs.Commands;
 
 public class CreateLocationCommand
 {
-    [Required(ErrorMessage = "Location name is required")]
-    [StringLength(100, MinimumLength = 1, ErrorMessage = "Location name must be between 1 and 100 characters")]
     public required string Name { get; set; }
 }
 
